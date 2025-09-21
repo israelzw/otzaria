34ff2dcfbcbcc2c12301445caf6a6e9c33a0cb1e
Y-PLONI <7353755@gmail.com>
2025-09-21 20:42:47 +0300

    עדכון מסך 'אודות התוכנה'

--- lib/navigation/about_screen.dart
+++ lib/navigation/about_screen.dart
@@ -13,6 +13,7 @@ class AboutScreen extends StatefulWidget {
 class _AboutScreenState extends State<AboutScreen> {
   String? appVersion;
   String? libraryVersion;
+  int? bookCount;
 
   @override
   void initState() {
@@ -55,174 +56,193 @@ class _AboutScreenState extends State<AboutScreen> {
     if (libraryVersion == 'unknown') {
       libraryVersion = 'לא ידוע';
     }
+
+    // Load book count
+    bookCount = await dataService.getTotalBookCount();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // Upper part with icon and title
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/icon/icon.png',
-                width: 128,
-                height: 128,
-              ),
-              const SizedBox(height: 16),
-              Text.rich(
-                TextSpan(
-                  text: 'אוצריא ',
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Upper part with icon and title
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/icon/icon.png',
+                  width: 128,
+                  height: 128,
+                ),
+                const SizedBox(height: 16),
+                Text.rich(
+                  TextSpan(
+                    text: 'אוצריא ',
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'מאגר תורני חינמי',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ],
                   ),
+                ),
+                const SizedBox(height: 32),
+                Wrap(
+                  alignment: WrapAlignment.center,
+                  crossAxisAlignment: WrapCrossAlignment.center,
                   children: [
-                    TextSpan(
-                      text: 'מאגר תורני חינמי',
+                    const Text(
+                      'תוכנה זו נוצרה והוקדשה על ידי: ',
                       style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.grey[700],
+                        fontSize: 16,
                       ),
                     ),
+                    _buildContributor('siivan22', 'https://github.com/Sivan22'),
+                    const Text(', '),
+                    _buildContributor('Y.PL', 'https://github.com/Y-PLONI'),
+                    const Text(', '),
+                    _buildContributor('YOSEFTT', 'https://github.com/YOSEFTT'),
+                    const Text(', '),
+                    _buildContributor(
+                        'zevisvei', 'https://github.com/zevisvei'),
                   ],
                 ),
-              ),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'תוכנה זו נוצרה והוקדשה על ידי: ',
-                    style: TextStyle(
-                      fontSize: 16,
+                const SizedBox(height: 16),
+                const Divider(color: Colors.grey),
+                const SizedBox(height: 16),
+                Column(
+                  children: [
+                    // Title aligned to the right
+                    Container(
+                      alignment: Alignment.centerRight,
+                      padding: const EdgeInsets.only(right: 16.0),
+                      child: const Text(
+                        'סכום משמעותי לפיתוח התוכנה, נתרם לעילוי נשמת:',
+                        style: TextStyle(fontSize: 16),
+                        textDirection: TextDirection.rtl,
+                      ),
                     ),
-                  ),
-                  _buildContributor('siivan22', 'https://github.com/Sivan22'),
-                  const Text(', '),
-                  _buildContributor('Y.PL', 'https://github.com/Y-PLONI'),
-                  const Text(', '),
-                  _buildContributor('YOSEFTT', 'https://github.com/YOSEFTT'),
-                  const Text(', '),
-                  _buildContributor('zevisvei', 'https://github.com/zevisvei'),
-                ],
-              ),
-              const SizedBox(height: 16),
-              const Divider(color: Colors.grey),
-              const SizedBox(height: 16),
-              Column(
-                children: [
-                  // Title aligned to the right
-                  Container(
-                    alignment: Alignment.centerRight,
-                    padding: const EdgeInsets.only(right: 16.0),
-                    child: const Text(
-                      'סכום משמעותי לפיתוח התוכנה, נתרם לעילוי נשמת:',
-                      style: TextStyle(fontSize: 16),
-                      textDirection: TextDirection.rtl,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
+                    const SizedBox(height: 16),
 
-                  // Scrollable and Centered Name
-                  SizedBox(
-                    height: 100, // Max height for the scrollable area
-                    child: SingleChildScrollView(
-                      child: Center(
-                        // Center the RichText
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          textDirection: TextDirection.rtl,
-                          text: const TextSpan(
-                            style: TextStyle(
-                              fontSize: 24, // Default size
-                              color: Colors.black,
+                    // Scrollable and Centered Name
+                    SizedBox(
+                      height: 100, // Max height for the scrollable area
+                      child: SingleChildScrollView(
+                        child: Center(
+                          // Center the RichText
+                          child: RichText(
+                            textAlign: TextAlign.center,
+                            textDirection: TextDirection.rtl,
+                            text: const TextSpan(
+                              style: TextStyle(
+                                fontSize: 24, // Default size
+                                color: Colors.black,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: 'ר\' ',
+                                  style: TextStyle(
+                                    fontSize: 20, // Smaller
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: 'משה',
+                                  style: TextStyle(
+                                    fontSize: 32, // Clearly larger
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: ' בן ',
+                                  style: TextStyle(
+                                    fontSize: 20, // Smaller
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: 'יהודה',
+                                  style: TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: ' ז"ל',
+                                  style: TextStyle(
+                                    fontSize: 20, // Smaller
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
                             ),
-                            children: [
-                              TextSpan(
-                                text: 'ר\' ',
-                                style: TextStyle(
-                                  fontSize: 20, // Smaller
-                                  fontWeight: FontWeight.normal,
-                                ),
-                              ),
-                              TextSpan(
-                                text: 'משה',
-                                style: TextStyle(
-                                  fontSize: 32, // Clearly larger
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              TextSpan(
-                                text: ' בן ',
-                                style: TextStyle(
-                                  fontSize: 20, // Smaller
-                                  fontWeight: FontWeight.normal,
-                                ),
-                              ),
-                              TextSpan(
-                                text: 'יהודה',
-                                style: TextStyle(
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              TextSpan(
-                                text: ' ז"ל',
-                                style: TextStyle(
-                                  fontSize: 20, // Smaller
-                                  fontWeight: FontWeight.normal,
-                                ),
-                              ),
-                            ],
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                  ],
+                ),
+              ],
+            ),
 
-          // Lower part with versions and donations
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'גרסת תוכנה: ${appVersion ?? 'לא ידוע'}',
-                style: const TextStyle(fontSize: 14),
-              ),
-              InkWell(
-                onTap: () async {
-                  const url =
-                      'https://docs.google.com/forms/d/e/1FAIpQLSeMrkGc0Jm_QMRfHB9c4LAmJtad8miEav6duEKhKqxbHK0pBg/viewform?usp=dialog';
-                  final uri = Uri.parse(url);
-                  if (await canLaunchUrl(uri)) {
-                    await launchUrl(uri);
-                  }
-                },
-                child: const Text(
-                  'לתרומות והנצחות',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    decoration: TextDecoration.underline,
-                    fontSize: 16,
+            // Lower part with versions and donations
+            Column(
+              children: [
+                InkWell(
+                  onTap: () async {
+                    const url = 'https://forms.gle/Dq8bn7mw7he4wtTC9';
+                    final uri = Uri.parse(url);
+                    if (await canLaunchUrl(uri)) {
+                      await launchUrl(uri);
+                    }
+                  },
+                  child: const Text(
+                    'לתרומות והנצחות, ולסיוע למאגר הספרים של אוצריא [פשוט וקל, כל אחד יכול!]',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                    softWrap: true,
                   ),
                 ),
-              ),
-              Text(
-                'גרסת ספריה: ${libraryVersion ?? 'לא ידוע'}',
-                style: const TextStyle(fontSize: 14),
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'גרסת תוכנה: ${appVersion ?? 'לא ידוע'}',
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                    Text(
+                      'גרסת ספריה: ${libraryVersion ?? 'לא ידוע'}',
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Center(
+                  child: Text(
+                    'מספר הספרים שבמאגר כעת: ${bookCount ?? 'לא ידוע'}',
+                    style: const TextStyle(fontSize: 14),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
